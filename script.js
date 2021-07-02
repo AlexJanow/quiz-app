178db0c71582d94bdbad69af876ea00719bc2ff9
AlexJanow <85886307+AlexJanow@users.noreply.github.com>
2021-07-02 15:00:40 +0200

    add answer toggle and edit re-toggle of bookmark

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ for (let i = 0; i < bookmark.length; i++) {
     bookmarkFilled[i].classList.toggle("hidden");
   });
 }
+
+const btn = document.querySelectorAll(".button-answer");
+const answer = document.querySelectorAll(".answer");
+
+for (let j = 0; j < btn.length; j++) {
+  btn[j].addEventListener("click", () => {
+    answer[j].classList.toggle("visible");
+  });
+}
